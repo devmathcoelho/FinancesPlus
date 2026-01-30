baecf6e6ad86dc6dd7a0f526009ffd9f2faf287e
devMathCoelho <mcoelho009@gmail.com>
2026-01-30 11:51:00 -0300

    fix: added bootstrap, deleted SSR for now

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const routes: Routes = [
+  // This makes the dashboard the landing page
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  
+  // This defines the dashboard route
+  { path: 'dashboard', component: DashboardComponent },
+  
+  { path: '**', redirectTo: 'dashboard' }
+];
